0c028e53cded690a9056f3524537334013c20c90
Geoffrey <geo.galagaran@gmail.com>
2017-01-06 11:51:12 +0800

    Remade everything

--- canvas.js
+++ canvas.js
@@ -1,27 +1,121 @@
 var canvas = (function(){
-  var canvas = $('canvas')[0];
+  var canvas = document.querySelector('canvas');
   var ctx = canvas.getContext('2d');
 
-  //default dimensions
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
 
+  function setDimension(w, h){
+    ctx.canvas.width = w;
+    ctx.canvas.height = h;
+  }
+
+  function getDimension(){
+    return {width: ctx.canvas.width, height: ctx.canvas.height};
+  }
+
+  function clearCanvas(){
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+
+  function draw(callback){
+    clearCanvas();
+    callback(ctx);
+  }
+
   return {
-    setDimension: function(width, height){
-      canvas.width = width;
-      canvas.height = height;
-    },
-
-    getDimension: function(){
-      return {width: canvas.width, height: canvas.height};
-    },
-
-    draw: function(callback){
-      callback(ctx);
-    }
+    setDimension: setDimension,
+    getDimension: getDimension,
+    draw: draw
   }
 })();
 
-canvas.draw(function(ctx){
-  
-});
+
+
+// FRACTAL TREE
+// canvas.draw(function(ctx){
+//   var angle = Math.PI/4;
+//   ctx.fillStyle = "#FFa500";
+//   ctx.strokeStyle = "#00F";
+//
+//   function tree(len){
+//     ctx.beginPath();
+//     ctx.moveTo(0, 0);
+//     ctx.lineTo(0, -len);
+//     ctx.stroke();
+//     ctx.translate(0, -len);
+//
+//     if(len > 5){
+//       ctx.save();
+//       ctx.rotate(angle * Math.random());
+//       tree(len * 0.67);
+//       ctx.restore();
+//
+//       ctx.save();
+//       ctx.rotate(-angle * Math.random());
+//       tree(len * 0.67);
+//       ctx.restore();
+//     }
+//     else{
+//       ctx.fillRect(-2.5, -2.5, 5, 5);
+//     }
+//
+//   }
+//
+//   ctx.translate(ctx.canvas.width/2, ctx.canvas.height);
+//   tree(200);
+// });
+
+
+// SPRITE ANIMATION USING DRAWIMAGE()
+// canvas.draw(function(ctx){
+//   var img = document.createElement('img');
+//   img.src = 'test.png';
+//
+//   var spriteWidth = 119;
+//   var spriteHeight = 153;
+//
+//   img.addEventListener('load', function(){
+//     var cycle = 0;
+//
+//     setInterval(function(){
+//       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+//       ctx.drawImage(img, cycle * spriteWidth, 0, spriteWidth, spriteHeight, 50, 50, spriteWidth, spriteHeight);
+//
+//       cycle = (cycle + 1) % 10;
+//     }, 100);
+//
+//   });
+// });
+
+
+//PIE CHART USING ARC();
+// canvas.draw(function(ctx){
+//   var results = [
+//     {name: "Satisfied", count: 1043, color: "#999"},
+//     {name: "Neutral", count: 563, color: "#aaa"},
+//     {name: "Unsatisfied", count: 510, color: "#bbb"},
+//     {name: "No comment", count: 175, color: "#ccc"}
+//   ];
+//
+//   var total = results.reduce(function(sum, choice){
+//     return sum + choice.count;
+//   }, 0);
+//
+//   var startAngle = 0;
+//
+//
+//   results.forEach(function(result){
+//     var portion = (result.count/total) * Math.PI * 2;
+//
+//     ctx.beginPath();
+//     ctx.arc(150, 150, 100, startAngle, startAngle + portion);
+//     ctx.lineTo(150, 150);
+//
+//     ctx.fillStyle = result.color;
+//     ctx.fill();
+//
+//     startAngle += portion;
+//   });
+//
+// });
